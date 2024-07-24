2a1e35195453018bb13fccb0b2b5e3fe7592804a
David Anderson <davidanderson.ads2020@gmail.com>
2024-07-24 18:14:06 -0300

    Subindo projeto

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente david = new Cliente();
+        david.setNome("David");
+
+        Conta cc = new ContaCorrente(david);
+        Conta poupanca = new ContaPoupanca(david);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
+}
+
